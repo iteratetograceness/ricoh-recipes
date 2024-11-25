b2d7ea5e0c3b79503031724456a1be5690e71fc5
Grace Yun <graceyun@Graces-MacBook-Air.local>
2024-11-24 21:13:52 -0500

    Upgrade + config updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'egpeqgon9nqy0dab.public.blob.vercel-storage.com',
-            port: '',
-            pathname: '/**',
-          },
-        ],
-      },    
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'egpeqgon9nqy0dab.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    dynamicIO: true,
+    cacheLife: {},
+    inlineCss: true,
+    ppr: true,
+    reactCompiler: true,
+    typedRoutes: true,
+    useLightningcss: true,
+    webVitalsAttribution: ['CLS', 'FCP', 'FID', 'INP', 'LCP', 'TTFB'],
+  },
+}
 
 export default nextConfig;
